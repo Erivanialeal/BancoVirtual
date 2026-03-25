c5a077fc761a482e36b761b40de8627430bcb464
Erivania Leal <erivanial.s1507@gmail.com>
2026-03-25 20:20:49 -0300

    Estrutura inicial da classe Cliente com validações de Cpf e email

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,66 +1,61 @@
 package src.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Cliente {
-    // a pasta model é onde fica as classes de dominios(entidade)
-    // a pasta model é o sistema.
-    // model não sabe como salvar buscar ou mostrar dados.
+
+    // atributos
     private String nome;
     private String cpf;
-    private String tipoCliente;
-    private List<Conta> contas = new ArrayList<>();
+    private String email;
+    private Map<String, Conta> Contas;
 
-    // .trim remove os espaços inuteis ex: transforma " " em ""
-    // null verifica se a variavel é nula
-    // isEmpty Verifica se , apos verificar os espaços sobrou algum caracter
-    // regras de negocio o nome é obrigatorio
-    public Cliente(String nome, String cpf, String tipoCliente) {
-        if (nome == null || nome.trim().isEmpty()) {
-            // valida nome
-            throw new IllegalArgumentException("Nome é obrigatorio!");
-        }
-        // valida tipo de cliente
-        if (tipoCliente == null || tipoCliente.trim().isEmpty()) {
-            throw new IllegalArgumentException("Tipo de cliente é obrigatório");
-        }
-        if (!tipoCliente.equals("PF") && !tipoCliente.equals("PJ")) {
-            throw new IllegalArgumentException("Tipo de cliente inválido. Use PF ou PJ.");
-        }
-        // valida CPF ou CNPJ conforme o tipo
-        if (cpf == null || cpf.trim().isEmpty()) {
-            throw new IllegalArgumentException("CPF/CNPJ é obrigatorio");
-
-        }
-        // valida tamnaho do documento conforme o tipo
-        if (tipoCliente.equals("PF") && cpf.length() != 11) {
-            throw new IllegalArgumentException("CPF inválido. Deve conter 11 dígitos.");
-        }
-
-        if (tipoCliente.equals("PJ") && cpf.length() != 14) {
-            throw new IllegalArgumentException("CNPJ inválido. Deve conter 14 dígitos.");
-        }
-
+    // iniciar os atributos
+    public Cliente(String nome, String cpf, String email) {
         this.nome = nome;
         this.cpf = cpf;
-        this.tipoCliente = tipoCliente;
+        this.email = email;
+        this.Contas = new HashMap<>();
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getCpf() {
         return cpf;
     }
 
-    public String getTipoCliente() {
-        return tipoCliente;
+    public Map<String, Conta> geContas() {
+        return Contas;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String validarCpf() {
+        if (getCpf() == null || getCpf().isEmpty()) {
+            return "Erro: Cpf não pode ser vazio";
+        }
+        if (getCpf().length() != 11) {
+            return "Erro: Cpf precisar ter 11 digitos";
+        }
+        return "Cpf: valido";
+    }
+
+    public String atualizarEmail(String novoEmail) {
+        if (novoEmail == null || novoEmail.isEmpty()) {
+            return "Erro: Email não pode ser vazio!";
+        }
+        if (!novoEmail.contains("@")) {
+            return "Erro: Email precisa conter @";
+        }
+        this.email = novoEmail;
+        return "Email atualizado com sucesso!";
     }
 
 }
